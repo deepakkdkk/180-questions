8f7750d4a78bd5f144757d7e1666de4a8af18938
deepakkdkk <deepakkumardk2811@gmail.com>
2022-06-06 01:15:52 +0530

    rotate image done

--- Arrays.java
+++ Arrays.java
@@ -277,3 +277,30 @@ class Solution {
         return sell;    
     }
 }
+
+// 7. Rotate image 
+class Solution {
+    public void rotate(int[][] arr) {
+        // Transpose
+        for(int i = 0; i < arr.length; i++){
+         for(int j = i; j < arr.length; j++){
+            int temp = arr[i][j];
+            arr[i][j]= arr[j][i];
+            arr[j][i] = temp;
+         }
+      }
+      // reverse columns, row by row
+      for(int i = 0; i < arr.length; i++){
+         int li = 0;
+         int ri = arr[0].length - 1;
+         while(li <= ri){
+            int temp = arr[i][li];
+            arr[i][li] = arr[i][ri];
+            arr[i][ri] = temp;
+
+            li++;
+            ri--;
+         }
+      }
+    }
+}
